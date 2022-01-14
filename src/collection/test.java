fad7a191e45c853cf7a6ff3c90856213ee50545c
chaewon <doingengineer@gmail.com>
2022-01-14 23:44:11 +0900

    first commit

---
+++ src/collection/test.java
@@ -0,0 +1,14 @@
+package collection;
+
+public class test {
+
+	public static void main(String[] args) {
+		//컬렉션 프레임웍
+		//컬렉션은 다수의 객체를 다루기 위한 표준화된 프로그래밍 방식
+		//컬렉션 프레임웍은 여러 객체를 다루는 방법을 정해진 대로 하는것
+		//객체를 다룬다 = 객체를 저장, 삭제, 검색, 정렬
+		//List, Set, Map
+
+	}
+
+}
---
+++ src/object_class/Hello2.java
@@ -0,0 +1,7 @@
+package object_class;
+
+//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직하다.
+public class Hello2 {} //소스파일의 이름은 public class 이름과 일치해야 한다.
+
+//public class Hello3 {} //하나의 소스파일에는 하나의 public class만 허용
+class Hello4 {}
